21c7f31505438c930047d8b24dd851502d02af08
MohammedGhouseP <ghousebashamail@gmail.com>
2024-08-06 22:57:37 +0530

    report

--- backend/index.js
+++ backend/index.js
@@ -1,88 +1,90 @@
-// const jsonServer = require("json-server");
-// const server = jsonServer.create();
-// const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 3000;
-
-// server.use(middlewares);
-// server.use(router);
-// server.listen(port);
-
-
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const fs = require('fs');
-const app = express();
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(cors());
+server.use(middlewares);
+server.use(router);
+server.listen(port);
 
-let tickets = [];
+// the above code is for the json-server
 
-// Load initial data from a JSON file (if you have one)
-const loadTickets = () => {
-  try {
-    const dataBuffer = fs.readFileSync('tickets.json');
-    tickets = JSON.parse(dataBuffer.toString());
-  } catch (e) {
-    console.log('No initial data found.');
-  }
-};
+// the below code is for the express server
 
-loadTickets();
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const fs = require('fs');
+// const app = express();
+// const port = process.env.PORT || 3000;
 
-// Save data to a JSON file
-const saveTickets = () => {
-  fs.writeFileSync('tickets.json', JSON.stringify(tickets));
-};
+// app.use(bodyParser.json());
+// app.use(cors());
 
-// Get all tickets
-app.get('/tickets', (req, res) => {
-  res.send(tickets);
-});
+// let tickets = [];
 
-// Create a new ticket
-app.post('/tickets', (req, res) => {
-  const newTicket = {
-    id: tickets.length + 1,
-    ...req.body
-  };
-  tickets.push(newTicket);
-  saveTickets();
-  res.status(201).send(newTicket);
-});
+// // Load initial data from a JSON file (if you have one)
+// const loadTickets = () => {
+//   try {
+//     const dataBuffer = fs.readFileSync('tickets.json');
+//     tickets = JSON.parse(dataBuffer.toString());
+//   } catch (e) {
+//     console.log('No initial data found.');
+//   }
+// };
 
-// Update a ticket
-app.put('/tickets/:id', (req, res) => {
-  const ticketId = parseInt(req.params.id);
-  const ticketIndex = tickets.findIndex(ticket => ticket.id === ticketId);
+// loadTickets();
 
-  if (ticketIndex !== -1) {
-    tickets[ticketIndex] = { id: ticketId, ...req.body };
-    saveTickets();
-    res.send(tickets[ticketIndex]);
-  } else {
-    res.status(404).send({ error: 'Ticket not found' });
-  }
-});
+// // Save data to a JSON file
+// const saveTickets = () => {
+//   fs.writeFileSync('tickets.json', JSON.stringify(tickets));
+// };
 
-// Delete a ticket
-app.delete('/tickets/:id', (req, res) => {
-  const ticketId = parseInt(req.params.id);
-  const ticketIndex = tickets.findIndex(ticket => ticket.id === ticketId);
+// // Get all tickets
+// app.get('/tickets', (req, res) => {
+//   res.send(tickets);
+// });
 
-  if (ticketIndex !== -1) {
-    const deletedTicket = tickets.splice(ticketIndex, 1);
-    saveTickets();
-    res.send(deletedTicket[0]);
-  } else {
-    res.status(404).send({ error: 'Ticket not found' });
-  }
-});
+// // Create a new ticket
+// app.post('/tickets', (req, res) => {
+//   const newTicket = {
+//     id: tickets.length + 1,
+//     ...req.body
+//   };
+//   tickets.push(newTicket);
+//   saveTickets();
+//   res.status(201).send(newTicket);
+// });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+// // Update a ticket
+// app.put('/tickets/:id', (req, res) => {
+//   const ticketId = parseInt(req.params.id);
+//   const ticketIndex = tickets.findIndex(ticket => ticket.id === ticketId);
+
+//   if (ticketIndex !== -1) {
+//     tickets[ticketIndex] = { id: ticketId, ...req.body };
+//     saveTickets();
+//     res.send(tickets[ticketIndex]);
+//   } else {
+//     res.status(404).send({ error: 'Ticket not found' });
+//   }
+// });
+
+// // Delete a ticket
+// app.delete('/tickets/:id', (req, res) => {
+//   const ticketId = parseInt(req.params.id);
+//   const ticketIndex = tickets.findIndex(ticket => ticket.id === ticketId);
+
+//   if (ticketIndex !== -1) {
+//     const deletedTicket = tickets.splice(ticketIndex, 1);
+//     saveTickets();
+//     res.send(deletedTicket[0]);
+//   } else {
+//     res.status(404).send({ error: 'Ticket not found' });
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
